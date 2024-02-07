92b0886ffe616a20e887adbb83f5fd5d5305de80
erikvdwiel <9019165@student.zadkine.nl>
2024-02-07 11:44:58 +0100

    Opdracht 1 gemaakt en pricepercubi toegevoegd

--- index/index.php
+++ index/index.php
@@ -7,34 +7,49 @@ class house {
    public $height;
    public $depth;
    public $volume;
-
+   public $price;
+   public $pricePerCubi = 800;
    
-   function __construct($floor, $rooms, $width, $height, $depth, $volume)
+   public function __construct($floor, $rooms, $width, $height, $depth)
    {
        $this->floor = $floor;
        $this->rooms = $rooms;
        $this->width = $width;
        $this->height = $height;
        $this->depth = $depth;
-       $this->volume = $volume;
+       $this->setVolume();
+       
    }
 
-}
+   public function setVolume(){
+     $this->volume = $this->width * $this->height * $this->depth;
+   }
 
+   public function getHouse(){
 
-$house1 = new house('', '', '', '', '', '');
-$house2 = new house('', '', '', '', '', '');
-$house3 = new house('', '', '', '', '', '');
+    return "Dit huis heeft {$this->floor} verdiepingen, {$this->rooms} kamers en heeft een volume van {$this->volume} m3";
 
-echo '<pre>';
-print_r($house1);
-echo '</pre>';
+   }
+   
 
-echo '<pre>';
-print_r($house2);
-echo '</pre>';
+   public function getPrice(){
 
-echo '<pre>';
-print_r($house3);
-echo '</pre>';
+   return "De prijs van het huis is " . ($this->volume * $this->pricePerCubi);
+    
+   }
 
+}   
+    
+
+$house1 = new house(2, 4, 15, 15, 20);
+$house2 = new house(3, 5, 15, 15, 15);
+$house3 = new house(2, 3, 15, 15, 12);
+
+echo $house1->getHouse() . "</br>";
+echo $house1->getPrice() . "</br>";
+
+echo $house2->getHouse() . "</br>";
+echo $house2->getPrice() . "</br>";
+
+echo $house3->getHouse() . "</br>";
+echo $house3->getPrice() . "</br>";
